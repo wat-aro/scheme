c4b27f606030be0471471f46c024e80c7b84fbef
wat-aro <kazutas1008@gmail.com>
2017-03-06 09:00:00 +0900

    add boolBinop

--- app/Main.hs
+++ app/Main.hs
@@ -226,11 +226,31 @@ primitives = [("+", numericBinop (+)),
               ("remainder", numericBinop rem),
               ("symbol?", isSymbol),
               ("symbol->string", symbolToString),
-              ("string->symbol", stringToSymbol)]
+              ("string->symbol", stringToSymbol),
+              ("=", numBoolBinop (==)),
+              ("<", numBoolBinop (<)),
+              (">", numBoolBinop (>)),
+              ("/=", numBoolBinop (/=)),
+              (">=", numBoolBinop (>=)),
+              ("&&", boolBoolBinop (&&)),
+              ("||", boolBoolBinop (||)),
+              ("string=?", strBoolBinop (==)),
+              ("string<?", strBoolBinop (<)),
+              ("string>?", strBoolBinop (>)),
+              ("string<=?", strBoolBinop (<=)),
+              ("string>=?", strBoolBinop (>=))
+              ]
 
 numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
 numericBinop op singleVal@[_] = throwError $ NumArgs 2 singleVal
 numericBinop op params = mapM unpackNum params >>= return . Number . foldl1 op
+
+boolBinop :: (LispVal -> ThrowsError a) -> (a -> a -> Bool) -> [LispVal]  -> ThrowsError LispVal
+boolBinop unpacker op args = if length args /= 2
+                             then throwError $ NumArgs 2 args
+                                  else do left K- unpacker $ head args
+                                          right <- unpacker $ args !! 1
+                                          return $ Bool $ left `op` right
 
 unpackNum :: LispVal -> ThrowsError Integer
 unpackNum (Number n) = return n
